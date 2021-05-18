5f12eae8ef3efff93cb9031d46f33b225b7afca0
Agustin Souto <agus.souto75@gmail.com>
2021-05-18 18:55:29 -0300

    Configurated Sequelize to Work with the Database . On app.js, added a get method for '/', which adds a post to the database. This endpoint will be edited to accomplish the challenge requirements. . Created a Post model, that defines the information that require a post. . package-lock and package modifications.

--- app.js
+++ app.js
@@ -1,14 +1,33 @@
 const express = require('express');
 const app = express();
 const sequelize = require('./database/database');
+const Post = require('./database/models/Post');
 
 // setting
 const PORT = process.env.PORT || 3000;
 
 app.get('/', function(req, res){
-    res.send('Hello to the Alkemy"s Blog');
+    
+    Post.create({
+        id: 1,
+        title: "Marco Reus: From the top to the injuries.",
+        contents: "The german player, is one of the top level players on the world. But he has to face constantly with injuries that don't let him play.",
+        image: "https://i.pinimg.com/originals/7c/da/a6/7cdaa6ebd3aba812041550643b2c7176.png",
+        category: 1
+    }).then(p => {
+        res.json(p);
+    });
+
 });
 
 app.listen(PORT, function(){
     console.log('This is the start of the project!');
+
+    // connect to the database
+    // force: TRUE (DROP TABLE) || FALSE (NO DROP)
+    sequelize.sync({ force: false }).then(() => {
+        console.log('Connected to the database!')
+    }).catch(error => {
+        console.log('Error: ', error);
+    })
 });
