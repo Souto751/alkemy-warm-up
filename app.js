12a1624c5ae45daa059b34d744bca406ab873b3f
Agustin Souto <agus.souto75@gmail.com>
2021-05-18 20:07:29 -0300

    Modified Save Post Endpoint . Changed the endpoint type from get to post, regarding to the challenge's requeriments. . Changed Post.create attributes.

--- app.js
+++ app.js
@@ -1,25 +1,29 @@
+// Import
 const express = require('express');
 const app = express();
 const sequelize = require('./database/database');
 const Post = require('./database/models/Post');
 
-// setting
+// port configuration
 const PORT = process.env.PORT || 3000;
 
-app.get('/', function(req, res){
-    
+// -- Endpoints Start -- //
+
+// Create New Post Endpoint
+app.post('/posts', function(req, res){
     Post.create({
-        id: 1,
-        title: "Marco Reus: From the top to the injuries.",
-        contents: "The german player, is one of the top level players on the world. But he has to face constantly with injuries that don't let him play.",
-        image: "https://i.pinimg.com/originals/7c/da/a6/7cdaa6ebd3aba812041550643b2c7176.png",
-        category: 1
+        title: req.query.title,
+        contents: req.query.contents,
+        image: req.query.image,
+        category: req.query.category
     }).then(p => {
         res.json(p);
     });
 
 });
+// -- Endpoints End -- //
 
+// Start the App
 app.listen(PORT, function(){
     console.log('This is the start of the project!');
 
