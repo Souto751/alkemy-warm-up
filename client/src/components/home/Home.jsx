3b9952ffb3832136f6df21b414e5938998ac61e0
Agustin Souto <agus.souto75@gmail.com>
2021-05-20 14:32:17 -0300

    Imported Header into Home to display it at Home . Also added id for the Home div, for css.

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -1,9 +1,17 @@
+// Import React
 import React from 'react'
 
+// Import Style
+import '../../style/Home.css';
+
+// Import Components
+import Header from '../header/Header';
+
 export default function Home() {
+    
     return (
-        <div>
-            
+        <div id="home">
+            <Header />
         </div>
     )
 }
