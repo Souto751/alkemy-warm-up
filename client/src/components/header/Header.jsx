bec9611a9e331872d06dfd99d5d5023ccd5a0399
Agustin Souto <agus.souto75@gmail.com>
2021-05-20 15:29:29 -0300

    Functional header with title and Links

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+// Import React
+import React from 'react';
+import { Link } from 'react-router-dom'
 
+// Import Style
+import '../../style/Header.css';
+
+// Export Component
 export default function Header() {
     return (
-        <div>
-            
+        <div id="header">
+            <h1>Blog</h1>
+            <div className="header-links">
+                <Link to="/" style={{ textDecoration: 'none' }}><p className="header-link">Home</p></Link>
+                <Link to="/create" style={{ textDecoration: 'none' }}><p className="header-link">Post</p></Link>
+            </div>
         </div>
     )
 }
