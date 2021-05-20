0c0825823257308c7ce82a58a5e7e2cad8c76986
Agustin Souto <agus.souto75@gmail.com>
2021-05-20 15:27:38 -0300

    Added routes into App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,26 @@
+// Import React
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+
 // Import Style
 import './style/Global.css';
 
 // Import Components
-import Home from './components/home/Home';
+import Home from './components/routes/Home';
+import Post from './components/post/Post';
+import Create from './components/routes/AddPost';
+import Update from './components/routes/UpdatePost';
 
 function App() {
     return (
         <div className="App">
-            <Home />
+            <Router>
+                <Switch>
+                    <Route exact path="/"><Home /></Route>
+                    <Route path="/post/:id"><Post /></Route>
+                    <Route path="/create"><Create /></Route>
+                    <Route path="/update"><Update /></Route>
+                </Switch>
+            </Router>
         </div>
     );
 }
