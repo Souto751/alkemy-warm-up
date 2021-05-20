c3341db21e8049f962cd6189340ea1bb597b9654
Agustin Souto <agus.souto75@gmail.com>
2021-05-20 17:07:27 -0300

    Modified Home component and Css to display posts

--- client/src/components/routes/Home.jsx
+++ client/src/components/routes/Home.jsx
@@ -6,6 +6,7 @@ import '../../style/Home.css';
 
 // Import Components
 import Header from '../header/Header';
+import Post from '../post/Post';
 
 // Export Component
 export default function Home() {
@@ -13,6 +14,13 @@ export default function Home() {
     return (
         <div id="home">
             <Header />
+            <div className="posts-div">
+                <Post />
+                <Post />
+                <Post />
+                <Post />
+                <Post />
+            </div>
         </div>
     )
 }
