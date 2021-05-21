c6c1e1feb8947fcbaa54c9fba0b266da24e248af
Agustin Souto <agus.souto75@gmail.com>
2021-05-21 10:20:41 -0300

    Added delete functionality that calls the API to delete the post with the sent id

--- client/src/components/post/Post.jsx
+++ client/src/components/post/Post.jsx
@@ -16,6 +16,17 @@ export default function Post(props) {
         localStorage.setItem('postId', props.props.id);
     }
 
+    const deletePost = () => {
+        selectId();
+        const idToDelete = localStorage.getItem('postId');
+        fetch('https://jsonplaceholder.typicode.com/posts/' + idToDelete, {
+            method: 'DELETE',
+        }).then(response => {
+            console.log(response);
+            alert("Post deleted successfully!");
+        });
+    }
+
     return (
         <div className="post">
             <div>
@@ -24,7 +35,7 @@ export default function Post(props) {
             <div className="post-options">
                 <Link to={"/post/" + props.props.id} style={{ textDecoration: 'none' }}><button className="post-link" onClick={() => selectId()}><FontAwesomeIcon icon={faLink} /></button></Link>
                 <Link to="/update"><button className="edit-post" onClick={() => selectId()}><FontAwesomeIcon icon={faEdit} color="black" /></button></Link>
-                <button className="delete-post"><FontAwesomeIcon icon={faTrashAlt} color="white" /></button>
+                <button className="delete-post" onClick={() => deletePost()}><FontAwesomeIcon icon={faTrashAlt} color="white" /></button>
             </div>
         </div>
     )
